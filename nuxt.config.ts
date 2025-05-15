fdc2bbd4dd91d0a2aab7f73ab50ac8675754bcc8
Owen Phan <phtsnh26@gmail.com>
2025-05-15 21:51:59 +0700

    release-v2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/',
+    baseURL: '/4tek-test',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
